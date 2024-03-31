117b47464540cfe4987db42ef7b0a935458fe39b
AjayNegi2023 <ajayn1803@gmail.com>
2024-03-31 12:42:20 +0530

    state management using redux

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './Login'
 import Browse from './Browse'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import {  onAuthStateChanged } from "firebase/auth";
+import { auth } from '../Utils/Firebase';
+import {useDispatch} from "react-redux";
+import { addUser, removeUser } from '../Utils/userSlice';
+
+
 
 const Body = () => {
-
+    const dispatch= useDispatch();
     const appRouter = createBrowserRouter([
        {
         path:"/",
@@ -14,7 +20,21 @@ const Body = () => {
         path:"/browse",
         element:<Browse/>
        }
-    ])
+    ]);
+
+    useEffect(()=>{
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const {uid,email,password,photoURL} = user.uid;
+          dispatch(addUser({uid:uid,email:email,password:password,photoURL:photoURL}));
+        } else {
+          // User is signed out
+          dispatch(removeUser());
+        }
+      }); 
+    },[]);
+
+
   return (
     <div>
         <RouterProvider router={appRouter} />
