2bf9c1a1c89898a918596a9cf21d26de0a717f7d
AjayNegi2023 <ajayn1803@gmail.com>
2024-04-01 19:31:16 +0530

    fetch data from TDMB

---
+++ src/Hooks/useNowPlayingMovies.js
@@ -0,0 +1,23 @@
+import React,{useEffect} from "react";
+import { API_OPTIONS } from "../Utils/Constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../Utils/movieSlice";
+
+const useNowPlayingMovies = ()=>{
+    const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async()=>{
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?page=1';
+    const data = await fetch(url, API_OPTIONS);
+    const JSON = await data.json();
+    console.log(JSON.results);
+    dispatch(addNowPlayingMovies(JSON.results));
+  }
+
+  useEffect(()=>{
+    getNowPlayingMovies();
+  },[])
+
+}
+
+export default useNowPlayingMovies;
