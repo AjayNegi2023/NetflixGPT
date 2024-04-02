6cc848c8f38e992e032a1b73692c5fd08d7939aa
AjayNegi2023 <ajayn1803@gmail.com>
2024-04-02 14:16:16 +0530

    Browse Container

---
+++ src/Components/MainContainer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import BackgroundVideo from './BackgroundVideo';
+import BackgroundTitle from './BackgroundTitle';
+
+
+const MainContainer = () => {
+
+    const movies = useSelector((store) => store.movies?.nowPlayingMovies)
+    if (movies === null) return;
+    const mainMovie = movies[0];
+
+    const { original_title, overview, id, key } = mainMovie;
+    return (
+        <div>
+            <BackgroundTitle title={original_title} overview={overview} />
+            <BackgroundVideo movieID={id} />
+        </div>
+    )
+}
+
+export default MainContainer
